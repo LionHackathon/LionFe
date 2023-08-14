8159b44b14c2b1513bba0b368730791cb9541e77
신성윤 <playword04@gmail.com>
2023-08-15 00:44:21 +0900

    except comment app, made all urls.py in apps

--- myAge/urls.py
+++ myAge/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post/',include('post.urls')),
+    path('question/',include('question.urls')),
+    path('user/',include('user.urls')),
+    path('answer/',include('answer.urls')),
 ]
