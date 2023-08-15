946311157aaf195cda2b4aeb1065037900e422d5
신성윤 <playword04@gmail.com>
2023-08-16 02:56:53 +0900

    feat: question model made, migrate them and  makesuperuser seongyun

--- question/models.py
+++ question/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class question(models.Model):
+    question_title = models.CharField(max_length = 50)
+    question_pub_date = models.DateTimeField("published date")
+    question_body = models.TextField()
+
+    def __str__(self):
+        return self.question_title
